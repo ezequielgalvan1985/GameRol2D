8925c28be5ef59122f1366804a2dd6ef2fa0eae4
ezgatrabajo <ezgatrabajo@gmail.com>
2020-04-18 10:57:56 -0300

    TiledPasto

---
+++ src/mapa/cuadro/TiledPasto.java
@@ -0,0 +1,19 @@
+package mapa.cuadro;
+
+import graficos.Pantalla;
+import graficos.Sprite;
+
+public class TiledPasto extends Tiled {
+    private int x;
+    private int y;
+    public Sprite sprite;
+
+    public TiledPasto(Sprite sprite){
+        super(sprite);
+    }
+
+    public void mostrar(int x, int y, Pantalla pantalla){
+
+    }
+
+}
